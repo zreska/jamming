9c33a0ac41be90b2c6088b84970827e6a7904142
Zack R <zreska@gmail.com>
2018-02-17 17:29:35 -0500

    Update SearchBar.js

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import './SearchBar.css'
+import './SearchBar.css';
 
-class SearchBar extends React.Component {
-  constructor(props){
+class SearchBar extends React.Component{
+  constructor(props) {
     super(props);
-
     this.state = {
-    term: ``
+      term: ''
     };
-    this.handleTermChange = this.handleTermChange.bind(this);
-    this.search = this.search.bind(this);
-    }
 
-    handleTermChange(event) {
-      this.setState({term: event.target.value});
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
   }
 
-    search() {
-      this.props.onSearch(this.state.term);
-    }
+  search() {
+    this.props.onSearch(this.state.term);
+  }
 
-  render(){
-    return(
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  render() {
+    return (
       <div className="SearchBar">
-      <input
-      placeholder="Enter A Song, Album, or Artist"
-      onChange={this.handleTermChange}
-      />
-      <a onClick={this.search}>SEARCH</a>
-</div>
+        <input
+          onChange={this.handleTermChange}
+          placeholder="Enter A Song, Album, or Artist" />
+        <a onClick={this.search}>SEARCH</a>
+      </div>
     );
   }
 }

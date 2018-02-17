570aff876c0fbe8d7b34f8d82edd6640d346f097
Zack R <zreska@gmail.com>
2018-02-17 17:30:09 -0500

    Update SearchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
+import TrackList from '../TrackList/TrackList';
 
-class SearchResults extends React.Component{
-  render(){
-    return(
+class SearchResults extends React.Component {
+  render() {
+    return (
       <div className="SearchResults">
-      <h2>Results</h2>
-      <TrackList
-      tracks={this.props.searchResults}
-      onAdd={this.props.onAdd}
-      isRemoval={false}/>
+        <h2>Results</h2>
+        <TrackList
+          isRemoval={false}
+          onAdd={this.props.onAdd}
+          tracks={this.props.searchResults} />
       </div>
     );
   }

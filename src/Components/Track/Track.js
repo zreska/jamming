dd891e357b593b78841b53cec461c94e0554c26f
Zack R <zreska@gmail.com>
2018-02-17 17:30:51 -0500

    Update Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import './Track.css';
 
-class Track extends React.Component{
-  constructor(props){
+class Track extends React.Component {
+  constructor(props) {
     super(props);
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
-    this.renderAction = this.renderAction.bind(this);
   }
 
-  renderAction(){
-    if(this.props.isRemoval){
-      return <a className="Track-action" onClick={this.removeTrack}>-</a>
-    }else{
-      return <a className="Track-action" onClick={this.addTrack}>+</a>
+  renderAction() {
+    if(this.props.isRemoval) {
+      return (<a className="Track-action" onClick={this.removeTrack}>-</a>);
+    } else {
+      return (<a className="Track-action" onClick={this.addTrack}>+</a>);
     }
   }
 
-  addTrack(){
+  addTrack() {
     this.props.onAdd(this.props.track);
   }
 
-  removeTrack(){
+  removeTrack() {
     this.props.onRemove(this.props.track);
   }
-  render(){
-    return(
+
+  render() {
+    return (
       <div className="Track">
-      <div className="Track-information">
-      <h3>{this.props.track.name}</h3>
-      <p>{this.props.track.artist} | {this.props.track.album}</p>
-      </div>
-      {this.renderAction()}
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
+        </div>
+        {this.renderAction()}
       </div>
     );
   }
